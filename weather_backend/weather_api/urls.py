affb9ec257bcd393fe44931cb68b9d0f5da9da58
Kiran Naragund <kirannaragund197@gmail.com>
2023-12-11 11:23:07 +0530

    Delete weather_backend directory

--- weather_backend/weather_api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import get_weather
-
-urlpatterns = [
-    path('api/weather', get_weather, name='get_weather'),
-    path('', get_weather, name='default_weather'),  
-]
